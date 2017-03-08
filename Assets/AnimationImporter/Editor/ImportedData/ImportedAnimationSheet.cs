ffea1713511834b00be29dd1ca51cd8debc56a26
RoniS <tonbery@gmail.com>
2017-03-08 16:15:23 -0300

    Project Upload.

---
+++ Assets/AnimationImporter/Editor/ImportedData/ImportedAnimationSheet.cs
@@ -0,0 +1,305 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using System.Linq;
+
+namespace AnimationImporter
+{
+	public class ImportedAnimationSheet
+	{
+		public string name { get; set; }
+		public string assetDirectory { get; set; }
+
+		public int width { get; set; }
+		public int height { get; set; }
+		public int maxTextureSize
+		{
+			get
+			{
+				return Mathf.Max(width, height);
+			}			
+		}
+
+		public List<ImportedAnimationFrame> frames = new List<ImportedAnimationFrame>();
+		public List<ImportedAnimation> animations = new List<ImportedAnimation>();
+
+		public bool hasAnimations
+		{
+			get
+			{
+				return animations != null && animations.Count > 0;
+			}
+		}
+
+		private Dictionary<string, ImportedAnimation> _animationDatabase = null;
+
+		private PreviousImportSettings _previousImportSettings = null;
+		public PreviousImportSettings previousImportSettings
+		{
+			get
+			{
+				return _previousImportSettings;
+			}
+			set
+			{
+				_previousImportSettings = value;
+			}
+		}
+		public bool hasPreviousTextureImportSettings
+		{
+			get
+			{
+				return _previousImportSettings != null && _previousImportSettings.hasPreviousTextureImportSettings;
+			}
+		}
+
+		// ================================================================================
+		//  public methods
+		// --------------------------------------------------------------------------------
+
+		// get animation by name; used when updating an existing AnimatorController 
+		public AnimationClip GetClip(string clipName)
+		{
+			if (_animationDatabase == null)
+				BuildIndex();
+
+			if (_animationDatabase.ContainsKey(clipName))
+				return _animationDatabase[clipName].animationClip;
+
+			return null;
+		}
+
+		/* 
+			get animation by name; used when creating an AnimatorOverrideController
+			we look for similar names so the OverrideController is still functional in cases where more specific or alternative animations are not present
+			idle <- idle
+			idleAlt <- idle
+		*/
+		public AnimationClip GetClipOrSimilar(string clipName)
+		{
+			AnimationClip clip = GetClip(clipName);
+
+			if (clip != null)
+				return clip;
+
+			List<ImportedAnimation> similarAnimations = new List<ImportedAnimation>();
+			foreach (var item in animations)
+			{
+				if (clipName.Contains(item.name))
+					similarAnimations.Add(item);
+			}
+
+			if (similarAnimations.Count > 0)
+			{
+				ImportedAnimation similar = similarAnimations.OrderBy(x => x.name.Length).Reverse().First();
+				return similar.animationClip;
+			}
+
+			return null;
+		}
+
+		public void CreateAnimation(ImportedAnimation anim, string basePath, string masterName, AnimationTargetObjectType targetType)
+		{
+			AnimationClip clip;
+            string fileName = basePath + "/" + masterName + "_" + anim.name + ".anim";
+			bool isLooping = anim.isLooping;
+
+			// check if animation file already exists
+			clip = AssetDatabase.LoadAssetAtPath<AnimationClip>(fileName);
+			if (clip != null)
+			{
+				// get previous animation settings
+				targetType = PreviousImportSettings.GetAnimationTargetFromExistingClip(clip);
+				isLooping = clip.isLooping;
+			}
+			else
+			{
+				clip = new AnimationClip();
+				AssetDatabase.CreateAsset(clip, fileName);
+			}
+
+			// change loop settings
+			if (isLooping)
+			{
+				clip.wrapMode = WrapMode.Loop;
+				clip.SetLoop(true);
+			}
+			else
+			{
+				clip.wrapMode = WrapMode.Clamp;
+				clip.SetLoop(false);
+			}
+
+			ObjectReferenceKeyframe[] keyFrames = new ObjectReferenceKeyframe[anim.Count + 1]; // one more than sprites because we repeat the last sprite
+
+			for (int i = 0; i < anim.Count; i++)
+			{
+				ObjectReferenceKeyframe keyFrame = new ObjectReferenceKeyframe { time = anim.GetKeyFrameTime(i) };
+
+				Sprite sprite = anim.frames[i].sprite;
+				keyFrame.value = sprite;
+				keyFrames[i] = keyFrame;
+			}
+
+			// repeating the last frame at a point "just before the end" so the animation gets its correct length
+
+			ObjectReferenceKeyframe lastKeyFrame = new ObjectReferenceKeyframe { time = anim.GetLastKeyFrameTime(clip.frameRate) };
+
+			Sprite lastSprite = anim.frames[anim.Count - 1].sprite;
+			lastKeyFrame.value = lastSprite;
+			keyFrames[anim.Count] = lastKeyFrame;
+
+			// save curve into clip, either for SpriteRenderer, Image, or both
+			if (targetType == AnimationTargetObjectType.SpriteRenderer)
+			{
+				AnimationUtility.SetObjectReferenceCurve(clip, AnimationClipUtility.spriteRendererCurveBinding, keyFrames);
+				AnimationUtility.SetObjectReferenceCurve(clip, AnimationClipUtility.imageCurveBinding, null);
+			}
+			else if (targetType == AnimationTargetObjectType.Image)
+			{
+				AnimationUtility.SetObjectReferenceCurve(clip, AnimationClipUtility.spriteRendererCurveBinding, null);
+				AnimationUtility.SetObjectReferenceCurve(clip, AnimationClipUtility.imageCurveBinding, keyFrames);
+			}
+			else if (targetType == AnimationTargetObjectType.SpriteRendererAndImage)
+			{
+				AnimationUtility.SetObjectReferenceCurve(clip, AnimationClipUtility.spriteRendererCurveBinding, keyFrames);
+				AnimationUtility.SetObjectReferenceCurve(clip, AnimationClipUtility.imageCurveBinding, keyFrames);
+			}
+
+			EditorUtility.SetDirty(clip);
+			anim.animationClip = clip;
+		}
+
+		public void ApplyGlobalFramesToAnimationFrames()
+		{
+			for (int i = 0; i < animations.Count; i++)
+			{
+				ImportedAnimation anim = animations[i];
+
+				anim.SetFrames(frames.GetRange(anim.firstSpriteIndex, anim.Count).ToArray());
+			}
+		}
+
+		// ================================================================================
+		//  determine looping state of animations
+		// --------------------------------------------------------------------------------
+
+		public void SetNonLoopingAnimations(List<string> nonLoopingAnimationNames)
+		{
+			Regex nonLoopingAnimationsRegex = GetRegexFromNonLoopingAnimationNames(nonLoopingAnimationNames);
+
+			foreach (var item in animations)
+			{
+				item.isLooping = ShouldLoop(nonLoopingAnimationsRegex, item.name);
+			}
+		}
+
+		private bool ShouldLoop(Regex nonLoopingAnimationsRegex, string name)
+		{
+			if (!string.IsNullOrEmpty(nonLoopingAnimationsRegex.ToString()))
+			{
+				if (nonLoopingAnimationsRegex.IsMatch(name))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		private Regex GetRegexFromNonLoopingAnimationNames(List<string> value)
+		{
+			string regexString = string.Empty;
+			if (value.Count > 0)
+			{
+				// Add word boundaries to treat non-regular expressions as exact names
+				regexString = string.Concat("\\b", value[0], "\\b");
+			}
+
+			for (int i = 1; i < value.Count; i++)
+			{
+				string anim = value[i];
+				// Add or to speed up the test rather than building N regular expressions
+				regexString = string.Concat(regexString, "|", "\\b", anim, "\\b");
+			}
+
+			return new System.Text.RegularExpressions.Regex(regexString);
+		}
+
+		// ================================================================================
+		//  Sprite Data
+		// --------------------------------------------------------------------------------
+
+		public SpriteMetaData[] GetSpriteSheet(SpriteAlignment spriteAlignment, float customX, float customY)
+		{
+			SpriteMetaData[] metaData = new SpriteMetaData[frames.Count];
+
+			for (int i = 0; i < frames.Count; i++)
+			{
+				ImportedAnimationFrame spriteInfo = frames[i];
+				SpriteMetaData spriteMetaData = new SpriteMetaData();
+
+				// sprite alignment
+				spriteMetaData.alignment = (int)spriteAlignment;
+				if (spriteAlignment == SpriteAlignment.Custom)
+				{
+					spriteMetaData.pivot.x = customX;
+					spriteMetaData.pivot.y = customY;
+				}
+
+				spriteMetaData.name = spriteInfo.name;
+				spriteMetaData.rect = new Rect(spriteInfo.x, spriteInfo.y, spriteInfo.width, spriteInfo.height);
+
+				metaData[i] = spriteMetaData;
+			}
+
+			return metaData;
+		}
+
+		public void ApplyCreatedSprites(Sprite[] sprites)
+		{
+			if (sprites == null)
+			{
+				return;
+			}
+
+			// apply to general list
+			for (int i = 0; i < sprites.Length; i++)
+			{
+				frames[i].sprite = sprites[i];
+			}
+
+			// apply to frames in animations (might be different classes than in general list)
+			foreach (var anim in animations)
+			{
+				for (int i = 0; i < anim.Count; i++)
+				{
+					int index = anim.firstSpriteIndex + i;
+
+					if (index < sprites.Length)
+					{
+						anim.frames[i].sprite = sprites[index];
+					}
+				}
+			}
+		}
+
+		// ================================================================================
+		//  private methods
+		// --------------------------------------------------------------------------------
+
+		private void BuildIndex()
+		{
+			_animationDatabase = new Dictionary<string, ImportedAnimation>();
+
+			for (int i = 0; i < animations.Count; i++)
+			{
+				ImportedAnimation anim = animations[i];
+				_animationDatabase[anim.name] = anim;
+			}
+		}
+	}
+}
---
+++ Assets/CharacterController_Demo/scripts/DemoScene.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prime31;
+
+
+public class DemoScene : MonoBehaviour
+{
+	// movement config
+	public float gravity = -25f;
+	public float runSpeed = 8f;
+	public float groundDamping = 20f; // how fast do we change direction? higher means faster
+	public float inAirDamping = 5f;
+	public float jumpHeight = 3f;
+
+	[HideInInspector]
+	private float normalizedHorizontalSpeed = 0;
+
+	private CharacterController2D _controller;
+	private Animator _animator;
+	private RaycastHit2D _lastControllerColliderHit;
+	private Vector3 _velocity;
+
+
+	void Awake()
+	{
+		_animator = GetComponent<Animator>();
+		_controller = GetComponent<CharacterController2D>();
+
+		// listen to some events for illustration purposes
+		_controller.onControllerCollidedEvent += onControllerCollider;
+		_controller.onTriggerEnterEvent += onTriggerEnterEvent;
+		_controller.onTriggerExitEvent += onTriggerExitEvent;
+	}
+
+
+	#region Event Listeners
+
+	void onControllerCollider( RaycastHit2D hit )
+	{
+		// bail out on plain old ground hits cause they arent very interesting
+		if( hit.normal.y == 1f )
+			return;
+
+		// logs any collider hits if uncommented. it gets noisy so it is commented out for the demo
+		//Debug.Log( "flags: " + _controller.collisionState + ", hit.normal: " + hit.normal );
+	}
+
+
+	void onTriggerEnterEvent( Collider2D col )
+	{
+		Debug.Log( "onTriggerEnterEvent: " + col.gameObject.name );
+	}
+
+
+	void onTriggerExitEvent( Collider2D col )
+	{
+		Debug.Log( "onTriggerExitEvent: " + col.gameObject.name );
+	}
+
+	#endregion
+
+
+	// the Update loop contains a very simple example of moving the character around and controlling the animation
+	void Update()
+	{
+
+        if (_controller.isGrounded)
+        {
+            if(_controller.velocity.magnitude > 0.1f)
+            {
+                _animator.Play("Run");
+            }
+            else
+            {
+                _animator.Play("Idle");
+            }
+        }
+        else
+        {
+            if(_controller.velocity.y > 0)
+            {
+                _animator.Play("Jump");
+            }
+            else
+            {
+                _animator.Play("Fall");
+            }
+        }
+
+		if( _controller.isGrounded )
+			_velocity.y = 0;
+
+		if( Input.GetKey( KeyCode.RightArrow ) )
+		{
+			normalizedHorizontalSpeed = 1;
+			if( transform.localScale.x < 0f )
+				transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
+
+			
+		}
+		else if( Input.GetKey( KeyCode.LeftArrow ) )
+		{
+			normalizedHorizontalSpeed = -1;
+			if( transform.localScale.x > 0f )
+				transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
+
+			
+		}
+		else
+		{
+			normalizedHorizontalSpeed = 0;
+
+			
+		}
+
+
+		// we can only jump whilst grounded
+		if( _controller.isGrounded && Input.GetKeyDown( KeyCode.UpArrow ) )
+		{
+			_velocity.y = Mathf.Sqrt( 2f * jumpHeight * -gravity );
+			
+		}
+
+
+		// apply horizontal speed smoothing it. dont really do this with Lerp. Use SmoothDamp or something that provides more control
+		var smoothedMovementFactor = _controller.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
+		_velocity.x = Mathf.Lerp( _velocity.x, normalizedHorizontalSpeed * runSpeed, Time.deltaTime * smoothedMovementFactor );
+
+		// apply gravity before moving
+		_velocity.y += gravity * Time.deltaTime;
+
+		// if holding down bump up our movement amount and turn off one way platform detection for a frame.
+		// this lets us jump down through one way platforms
+		if( _controller.isGrounded && Input.GetKey( KeyCode.DownArrow ) )
+		{
+			_velocity.y *= 3f;
+			_controller.ignoreOneWayPlatformsThisFrame = true;
+		}
+
+		_controller.move( _velocity * Time.deltaTime );
+
+		// grab our current _velocity to use as a base for all calculations
+		_velocity = _controller.velocity;
+	}
+
+}
